0a08958404a237bb66ee4b851555b2a6e7e3c8b3
danix012 <72176313+danix012@users.noreply.github.com>
2020-11-02 13:37:42 +1300

    HomePage
    navbar and header first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
+import React from 'react';
+import logo from './Star Logo 07-2.png';
+import heroimage from './Hero image option 1 edit 2.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+    <div className="container">
+
+      <div className="topnav">
+
+        <div className="logo">
+          <img src={logo} />
+        </div>
+
+        <div className="links">
+          <a href="#">HOME</a>
+          <a href="#">FEATURES</a>
+          <a href="#">TEACHERS</a>
+        </div>
+
+        <div className="langLog">
+
+          <button>lang+flags</button>
+          <button>Register | Log in</button>
+
+        </div>
+
+      </div>
+      <header className="appHeader">
+        <img src={heroimage} />
+        <div className="headerText">
+          <p>Prepare young minds for a better <span>future.</span></p>
+          <p>Let us help you advance students in Digital Technologies and other learning areas with our project-based learning programme.</p>
+          <div className="headerButtons">
+            <button className="LearnMore">Learn More</button>
+            <button className="signUp">Sign Up</button>
+          </div>
+        </div>
+
       </header>
     </div>
+
   );
 }
 
